f2908bbf9db037306aa1a8272a8d8b982965dea1
lord-amresh <amresh2006@gmail.com>
2026-05-03 15:25:10 +0545

    Banner completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Banner from '../components/Banner'
 import Navbar from '../components/Navbar'
 
 const Home = () => {
   return (
     <div>
-        <Navbar/>
+        <Navbar />
+        <Banner />
     </div>
   )
 }
