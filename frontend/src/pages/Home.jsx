f4dee75fbc62f8a293566da13592ee22810d8ae6
lord-amresh <amresh2006@gmail.com>
2026-05-06 12:51:43 +0545

    home courses completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../components/Banner'
 import Navbar from '../components/Navbar'
+import HomeCourses from '../components/HomeCourses'
 
 const Home = () => {
   return (
     <div>
         <Navbar />
         <Banner />
+        <HomeCourses/>
     </div>
   )
 }
