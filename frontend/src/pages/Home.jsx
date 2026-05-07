b9d589d24af26a1acffbcbc412d5336e71abb19d
lord-amresh <amresh2006@gmail.com>
2026-05-07 14:35:18 +0545

    completed homecourses , testimonials

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Banner from '../components/Banner'
 import Navbar from '../components/Navbar'
 import HomeCourses from '../components/HomeCourses'
+import Testimonial from '../components/Testimonial'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <div>
         <Navbar />
         <Banner />
-        <HomeCourses/>
+        <HomeCourses />
+        <Testimonial />
+        <Footer />
     </div>
   )
 }
