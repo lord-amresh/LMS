328c2ab4302cd6fd432ec835224c2893936071a3
lord-amresh <amresh2006@gmail.com>
2026-05-09 22:46:54 +0545

    backend start

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+const port = 4000;
+
+// MIDDLEWARES
+
+// ROUTES
+
+// APP PORT AND LISTEN
+app.get('/', (req, res) => {
+    res.send('API WORKING');
+});
+
+app.listen(port, () => {
+    console.log(`Server Started on http://localhost:${port}`);
+})
