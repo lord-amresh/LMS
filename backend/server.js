4e8cdf6297170f186da9c65d6ee0367188a0b5f0
lord-amresh <amresh2006@gmail.com>
2026-05-10 20:56:25 +0545

    Admin panel start

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ const app = express();
 const port = 4000;
 
 // MIDDLEWARES
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173', "http://localhost:5174"],
+    credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(clerkMiddleware());
