ff01de9620b3321c29148279dacd05e4416887e5
lord-amresh <amresh2006@gmail.com>
2026-05-10 07:10:34 +0545

    coursemodel created

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import { clerkMiddleware } from '@clerk/express'
+import { connect } from 'mongoose';
+import { connectDB } from './config/db.js';
 
 const app = express();
 const port = 4000;
 
 // MIDDLEWARES
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(clerkMiddleware());
+
+//  DB
+connectDB();
 
 // ROUTES
 
