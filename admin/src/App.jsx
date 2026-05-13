fcb3b55ae01d43e8f4eee3c2c6c109537a086192
lord-amresh <amresh2006@gmail.com>
2026-05-14 00:38:00 +0545

    Admin completed

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Add from "./pages/Add";
 import List from "./pages/List";
+import Bookings from "./pages/Bookings";
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/addcourse" element={<Add />} />
       <Route path="/listcourse" element={<List />} />
+      <Route path="/bookings" element={<Bookings />} />
     </Routes>
   );
 };
